544bafb74553a47a8abe35270942533f7004817b
vivekram123 <vivekram.techies123@gmail.com>
2021-05-07 18:19:15 +0530

    make changes

--- techbox/settings.py
+++ techbox/settings.py
@@ -95,7 +95,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'techbox.wsgi.application'
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
+sentry_sdk.init(
+    dsn="https://0f37ffedb4d145f295b18b8f11ee0617@o625349.ingest.sentry.io/5753779",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
